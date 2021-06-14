205665f9338f05f7a9dd218166c20b05d14c1135
Glen <glen15@naver.com>
2021-06-15 08:44:05 +0900

    new repository name

--- app0615.js
+++ app0615.js
@@ -1,3 +1,13 @@
 'use strict';
-
 console.log("hello world");
+
+// string, number, boolean, udefined 의미 이해//
+
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Data_structures
+const name = "glen"; //string
+const age = 30;      //number
+const good = true;    //boolean
+const sports = ["soccer", "baseball", "squash"]; //arry
+console.log(name);
+
+ 
